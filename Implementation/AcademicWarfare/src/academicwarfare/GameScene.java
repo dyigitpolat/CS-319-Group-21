90f1580bdb7a7a4fbe2089deaf15671f3f781ac9
Selin Fildiş <selinfildis@gmail.com>
2016-12-23 01:31:12 +0300

    GameScene Changed.
    GameItem + GameLabel added,
    
    To:Do: GameItem needs images :')

--- Implementation/AcademicWarfare/src/academicwarfare/GameScene.java
+++ Implementation/AcademicWarfare/src/academicwarfare/GameScene.java
@@ -5,6 +5,7 @@
  */
 package academicwarfare;
 
+import java.awt.BorderLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,8 +18,10 @@ import javax.swing.JLabel;
  */
 public class GameScene extends Scene
 {
-    private JLabel pointLabel;
-    private final String POINTS_DISPLAY_TEXT = "Points:";
+    private GameLabel pointLabel;
+    private GameLabel waveLabel;
+    private final String WAVE_DISPLAY_TEXT = "Wave: ";
+    private final String POINTS_DISPLAY_TEXT = "Points: ";
     public GameScene()
     {
         initScene();
@@ -32,15 +35,22 @@ public class GameScene extends Scene
     
     public final void initScene()
     {
-        pointLabel = new JLabel(POINTS_DISPLAY_TEXT + 0);
-        //pointLabel.setHorizontalAlignment(JLabel.RIGHT);
+        pointLabel = new GameLabel(POINTS_DISPLAY_TEXT + 0);
+        pointLabel.setPosition(new Vector2(600,30));
+        pointLabel.setSize(new Vector2(10000,5000));
+        addObject(pointLabel);
+        
+        waveLabel = new GameLabel(WAVE_DISPLAY_TEXT + 1);
+        waveLabel.setPosition(new Vector2 (600, 50));
+        waveLabel.setSize(new Vector2(1000,500));
+        addObject(waveLabel);
         
     }
     public void setPoints(int points){
-        pointLabel.setText(POINTS_DISPLAY_TEXT + points);
+        pointLabel.setString(POINTS_DISPLAY_TEXT + points);
     }
     public int getPoints(){
-        return Integer.parseInt(pointLabel.getText().split(" ")[1]);
+        return Integer.parseInt(pointLabel.getString().split(" ")[1]);
     }
 }
 
